3b7abe664bdf649fdfbbd64099b1cdfbfe4e9a5c
João Vicente Souto <joaovicentesouto@gmail.com>
2017-03-26 10:26:44 -0300

    Pilha, Fila e Lista concluídos

--- Fila/array_queue.h
+++ Fila/array_queue.h
@@ -8,161 +8,160 @@
 
 namespace structures {
 
-    template<typename T>
+//! Classe Fila
+/*! Implementação de uma fila, um dos tipos de estruturas de dados vistos
+ *  durante o sementre. A fila possui o comportamento de uma fila normal,
+ *  onde o primeiro que entre é o primeiro a sair.
+ *  \author João Vicente Souto.
+ *  \since 17/03/17
+ *  \version 1.0
+ */
+template<typename T>
+class ArrayQueue {
+ public:
+    ArrayQueue();
+    explicit ArrayQueue(std::size_t max);
+    ~ArrayQueue();
+    void enqueue(const T& data);
+    T dequeue();
+    T& back();
+    void clear();
+    std::size_t size();
+    std::size_t max_size();
+    bool empty();
+    bool full();
 
-    //! Classe Fila
-    /*! Implementação de uma fila, um dos tipos de estruturas de dados vistos
-     *  durante o sementre. A fila possui o comportamento de uma fila normal,
-     *  onde o primeiro que entre é o primeiro a sair.
-     *  \author João Vicente Souto.
-     *  \since 17/03/17
-     *  \version 1.0
-     */
-    class ArrayQueue {
-    public:
-        ArrayQueue();
-        explicit ArrayQueue(std::size_t max);
-        ~ArrayQueue();
-        void enqueue(const T& data);
-        T dequeue();
-        T& back();
-        void clear();
-        std::size_t size();
-        std::size_t max_size();
-        bool empty();
-        bool full();
+ private:
+    T* contents;  //!< Array de tipo genérico para armazenar a fila.
+    std::size_t size_;  //!< Tamanho atual da fila.
+    std::size_t max_size_;  //!< Tamanho máximo da fila.
+    static const auto DEFAULT_SIZE = 10u;   //!< Tamanho padrão da fila.
+};
 
-    private:
-        T* contents;  //!< Array de tipo genérico para armazenar a fila.
-        std::size_t size_;  //!< Tamanho atual da fila.
-        std::size_t max_size_;  //!< Tamanho máximo da fila.
-        static const auto DEFAULT_SIZE = 10u;   //!< Tamanho padrão da fila.
-    };
+//! Construtor padrão
+/*! Sem parâmetros, a fila é inicializada com valores padrões.
+ *  \sa ArrayStack(std::size_t max)
+ */
+template<typename T>
+ArrayQueue<T>::ArrayQueue() {
+    max_size_ = DEFAULT_SIZE;
+    contents = new T[max_size_];
+    size_ = -1;
+}
 
-    //! Construtor padrão
-    /*! Sem parâmetros, a fila é inicializada com valores padrões.
-     *  \sa ArrayStack(std::size_t max)
-     */
-    template<typename T>
-    ArrayQueue<T>::ArrayQueue() {
-        max_size_ = DEFAULT_SIZE;
-        contents = new T[max_size_];
-        size = -1;
+//! Construtor passando o tamanho da fila.
+/*! Usando parâmetro max o usuário pode criar uma fila de qualquer tamanho.
+ *  \param max um size_t contendo o tamanho da fila que deve ser criada.
+ *  \sa ArrayStack()
+ */
+template<typename T>
+ArrayQueue<T>::ArrayQueue(std::size_t max) {
+    max_size_ = max;
+    contents = new T[max_size_];
+    size_ = -1;
+}
+
+//! Destrutor
+/*! Método para desalocar a memória utilizada pela fila.
+ */
+template<typename T>
+ArrayQueue<T>::~ArrayQueue() {
+    delete[] contents;
+}
+
+//! Inserção de um novo dado na fila.
+/*! Sempre será colocado no final da fila, caso não esteja vazia.
+ *  \param data um novo dado que será inserido na fila.
+ *  \sa dequeue(), back(), clear()
+ */
+template<typename T>
+void ArrayQueue<T>::enqueue(const T& data) {
+    if (full())
+        throw std::out_of_range("Fila cheia!");
+    else
+        contents[++size_] = data;
+}
+
+//! Remoção de um dado da fila.
+/*! Sempre será removido o primeiro dado da fila, caso não esteja vazia,
+ *  e em seguida os dados são reorganizados uma posição a frente da atual.
+ *  \sa enqueue(), back(), clear()
+ */
+template<typename T>
+T ArrayQueue<T>::dequeue() {
+    if (empty()) {
+        throw std::out_of_range("Fila vazia!");
+    } else {
+        T tmp = contents[0];
+        for (int i = 0; i < static_cast<int>(size_); i++)
+            contents[i] = contents[i+1];
+        size_--;
+        return tmp;
     }
+}
 
-    //! Construtor passando o tamanho da fila.
-    /*! Usando parâmetro max o usuário pode criar uma fila de qualquer tamanho.
-     *  \param max um size_t contendo o tamanho da fila que deve ser criada.
-     *  \sa ArrayStack()
-     */
-    template<typename T>
-    ArrayQueue<T>::ArrayQueue(std::size_t max) {
-        max_size_ = max;
-        contents = new T[max_size_];
-        size_ = -1;
-    }
+//! Referencia o dado no final da fila.
+/*! Retorna o dado que esta no final da fila para uso externo, caso exista.
+ *  \return T& um dado que será referenciado.
+ *  \sa enqueue(), dequeue(), clear()
+ */
+template<typename T>
+T& ArrayQueue<T>::back() {
+    if (empty())
+        throw std::out_of_range("Fila vazia!");
+    else
+        return contents[size_];
+}
 
-    //! Destrutor
-    /*! Método para desalocar a memória utilizada pela fila.
-     */
-    template<typename T>
-    ArrayQueue<T>::~ArrayQueue() {
-        delete contents;
-    }
+//! Esvazia a fila.
+/*! Atualiza o tamanho da fila para -1, indicando que a fila esta vazia.
+ *  \sa enqueue(), dequeue(), back()
+ */
+template<typename T>
+void ArrayQueue<T>::clear() {
+    size_ = -1;
+}
 
-    //! Inserção de um novo dado na fila.
-    /*! Sempre será colocado no final da fila, caso não esteja vazia.
-     *  \param data um novo dado que será inserido na fila.
-     *  \sa dequeue(), back(), clear()
-     */
-    template<typename T>
-    void ArrayQueue<T>::enqueue(const T& data) {
-        if (full())
-            throw std::out_of_range("Fila cheia!");
-        else
-            contents[++size_] = data;
-    }
+//! Quantidade de dados na fila.
+/*! Retorna o tamanho da fila somando-se 1 porque a lógica do tamanho da
+ *  fila inicia em -1.
+ *  \return um size_t referente ao tamanho da fila.
+ *  \sa max_size()
+ */
+template<typename T>
+std::size_t ArrayQueue<T>::size() {
+    return size_+1;
+}
 
-    //! Remoção de um dado da fila.
-    /*! Sempre será removido o primeiro dado da fila, caso não esteja vazia,
-     *  e em seguida os dados são reorganizados uma posição a frente da atual.
-     *  \sa enqueue(), back(), clear()
-     */
-    template<typename T>
-    T ArrayQueue<T>::dequeue() {
-        if (empty())
-            throw std::out_of_range("Fila vazia!");
-        else {
-            T tmp = contents[0];
-            for (int i = 0; i < static_cast<int>(size_); i++)
-                contents[i] = contents[i+1];
-            size_--;
-            return tmp;
-        }
-    }
+//! Maior tamanho possível.
+/*! Retorna o maior tamanho possível armazenado na variavel max_size_.
+ *  \return um size_t referente ao max_size_.
+ *  \sa size()
+ */
+template<typename T>
+std::size_t ArrayQueue<T>::max_size() {
+    return max_size_;
+}
 
-    //! Referencia o dado no final da fila.
-    /*! Retorna o dado que esta no final da fila para uso externo, caso exista.
-     *  \return T7 um dado que será referenciado.
-     *  \sa enqueue(), dequeue(), clear()
-     */
-    template<typename T>
-    T& ArrayQueue<T>::back() {
-        if (empty())
-            throw std::out_of_range("Fila vazia!");
-        else
-            return contents[size_];
-    }
+//! Fila vazia
+/*! Testa se a fila está vazia.
+ *  \return um booleano.
+ *  \sa full()
+ */
+template<typename T>
+bool ArrayQueue<T>::empty() {
+    return static_cast<int>(size_) == -1;
+}
 
-    //! Esvazia a fila.
-    /*! Atualiza o tamanho da fila para -1, indicando que a fila esta vazia.
-     *  \sa enqueue(), dequeue(), back()
-     */
-    template<typename T>
-    void ArrayQueue<T>::clear() {
-        size_ = -1;
-    }
-
-    //! Quantidade de dados na fila.
-    /*! Retorna o tamanho da fila somando-se 1 porque a lógica do tamanho da
-     *  fila inicia em -1.
-     *  \return um size_t referente ao tamanho da fila.
-     *  \sa max_size()
-     */
-    template<typename T>
-    std::size_t ArrayQueue<T>::size() {
-        return size_+1;
-    }
-
-    //! Maior tamanho possível.
-    /*! Retorna o maior tamanho possível armazenado na variavel max_size_.
-     *  \return um size_t referente ao max_size_.
-     *  \sa size()
-     */
-    template<typename T>
-    std::size_t ArrayQueue<T>::max_size() {
-        return max_size_;
-    }
-
-    //! Fila vazia
-    /*! Testa se a fila está vazia.
-     *  \return um booleano.
-     *  \sa full()
-     */
-    template<typename T>
-    bool ArrayQueue<T>::empty() {
-        return static_cast<int>(size_) == -1;
-    }
-
-    //! Fila cheia
-    /*! Testa se a fila está cheia.
-     *  \return um booleano.
-     *  \sa empty()
-     */
-    template<typename T>
-    bool ArrayQueue<T>::full() {
-        return size_ == max_size_-1;
-    }
+//! Fila cheia
+/*! Testa se a fila está cheia.
+ *  \return um booleano.
+ *  \sa empty()
+ */
+template<typename T>
+bool ArrayQueue<T>::full() {
+    return size_ == max_size_-1;
+}
 
 }  // namespace structures
 
--- Pilha/array_stack.h
+++ Pilha/array_stack.h
@@ -8,158 +8,157 @@
 
 namespace structures {
 
-    template<typename T>
+//! Classe Pilha
+/*! Implementação de uma pilha, um dos tipos de estruturas de dados vistos
+ *  durante o sementre. Possui o comportamento de uma pilha de pratos, na
+ *  qual todas as formas de acesso e modificação se dão pelo dado que está
+ *  no topo da pilha.
+ *  \author João Vicente Souto.
+ *  \since 17/03/17
+ *  \version 1.0
+ */
+template<typename T>
+class ArrayStack {
+ public:
+    ArrayStack();
+    explicit ArrayStack(std::size_t max);
+    ~ArrayStack();
+    void push(const T& data);
+    T pop();
+    T& top();
+    void clear();
+    std::size_t size();
+    std::size_t max_size();
+    bool empty();
+    bool full();
 
-    //! Classe Pilha
-    /*! Implementação de uma pilha, um dos tipos de estruturas de dados vistos
-     *  durante o sementre. Possui o comportamento de uma pilha de pratos, na
-     *  qual todas as formas de acesso e modificação se dão pelo dado que está
-     *  no topo da pilha.
-     *  \author João Vicente Souto.
-     *  \since 17/03/17
-     *  \version 1.0
-     */
-    class ArrayStack {
-    public:
-        ArrayStack();
-        explicit ArrayStack(std::size_t max);
-        ~ArrayStack();
-        void push(const T& data);
-        T pop();
-        T& top();
-        void clear();
-        std::size_t size();
-        std::size_t max_size();
-        bool empty();
-        bool full();
+ private:
+    T* contents;  //!< Array de tipo genérico para armazenar a pilha.
+    int top_;     //!< Topo da pilha.
+    std::size_t max_size_;  //!< Tamanho máximo da pilha.
+    static const auto DEFAULT_SIZE = 10u;  //!< Tamanho padrão da pilha.
+};
 
-    private:
-        T* contents;  //!< Array de tipo genérico para armazenar a pilha.
-        int top_;     //!< Topo da pilha.
-        std::size_t max_size_;  //!< Tamanho máximo da pilha.
-        static const auto DEFAULT_SIZE = 10u;  //!< Tamanho padrão da pilha.
-    };
+//! Construtor padrão
+/*! Sem parâmetros, a pilha é inicializada com valores padrões.
+ *  \sa ArrayStack(std::size_t max)
+ */
+template<typename T>
+ArrayStack<T>::ArrayStack() {
+    max_size_ = DEFAULT_SIZE;
+    contents = new T[max_size_];
+    top_ = -1;
+}
 
-    //! Construtor padrão
-    /*! Sem parâmetros, a pilha é inicializada com valores padrões.
-     *  \sa ArrayStack(std::size_t max)
-     */
-    template<typename T>
-    ArrayStack<T>::ArrayStack() {
-        max_size_ = DEFAULT_SIZE;
-        contents = new T[max_size_];
-        top_ = -1;
-    }
+//! Construtor passando o tamanho da pilha.
+/*! Usando parâmetro max o usuário pode criar uma pilha de qualquer tamanho.
+ *  \param max um size_t contendo o tamanho da pilha que deve ser criada.
+ *  \sa ArrayStack()
+ */
+template<typename T>
+ArrayStack<T>::ArrayStack(std::size_t max) {
+    max_size_ = max;
+    contents = new T[max_size_];
+    top_ = -1;
+}
 
-    //! Construtor passando o tamanho da pilha.
-    /*! Usando parâmetro max o usuário pode criar uma pilha de qualquer tamanho.
-     *  \param max um size_t contendo o tamanho da pilha que deve ser criada.
-     *  \sa ArrayStack()
-     */
-    template<typename T>
-    ArrayStack<T>::ArrayStack(std::size_t max) {
-        max_size_ = max;
-        contents = new T[max_size_];
-        top_ = -1;
-    }
+//! Destrutor
+/*! Método para desalocar a memória utilizada pela pilha.
+ */
+template<typename T>
+ArrayStack<T>::~ArrayStack() {
+    delete[] contents;
+}
 
-    //! Destrutor
-    /*! Método para desalocar a memória utilizada pela pilha.
-     */
-    template<typename T>
-    ArrayStack<T>::~ArrayStack() {
-        delete contents;
-    }
+//! Inserção de um novo dado na pilha.
+/*! Se baseando no topo da pilha, será alocado na próxima posição o novo dado
+ *  e atualizar o índice do topo.
+ *  \param data un novo dado que será inserido na pilha.
+ *  \sa pop(), top(), clear()
+ */
+template<typename T>
+void ArrayStack<T>::push(const T& data) {
+    if (full())
+        throw std::out_of_range("Pilha cheia!");
+    else
+        contents[++top_] = data;
+}
 
-    //! Inserção de um novo dado na pilha.
-    /*! Se baseando no topo da pilha, será alocado na próxima posição o novo dado
-     *  e atualizar o índice do topo.
-     *  \param data un novo dado que será inserido na pilha.
-     *  \sa pop(), top(), clear()
-     */
-    template<typename T>
-    void ArrayStack<T>::push(const T& data) {
-        if (full())
-            throw std::out_of_range("Pilha cheia!");
-        else
-            contents[++top_] = data;
-    }
+//! Remoção de um dado da pilha.
+/*! Retira o dado que está no topo da pilha e atualizar índice do topo.
+ *  \sa push(), top(), clear()
+ */
+template<typename T>
+T ArrayStack<T>::pop() {
+    if (empty())
+        throw std::out_of_range("Pilha vazia!");
+    else
+        return contents[top_--];
+}
 
-    //! Remoção de um dado da pilha.
-    /*! Retira o dado que está no topo da pilha e atualizar índice do topo.
-     *  \sa push(), top(), clear()
-     */
-    template<typename T>
-    T ArrayStack<T>::pop() {
-        if (empty())
-            throw std::out_of_range("Pilha vazia!");
-        else
-            return contents[top_--];
-    }
+//! Referencia o dado no topo da pilha.
+/*! Retorna o dado do topo da pilha que será referenciado para uso externo,
+ *  caso exista.
+ *  \return T& um dado para ser referenciado.
+ *  \sa push(), pop(), clear()
+ */
+template<typename T>
+T& ArrayStack<T>::top() {
+    if (empty())
+        throw std::out_of_range("Pilha vazia!");
+    else
+        return contents[top_];
+}
 
-    //! Referencia o dado no topo da pilha.
-    /*! Retorna o dado do topo da pilha que será referenciado para uso externo,
-     *  caso exista.
-     *  \return T& um dado para ser referenciado.
-     *  \sa push(), pop(), clear()
-     */
-    template<typename T>
-    T& ArrayStack<T>::top() {
-        if (empty())
-            throw std::out_of_range("Pilha vazia!");
-        else
-            return contents[top_];
-    }
+//! Esvazia a pilha.
+/*! Atualiza o índice do topo para -1, indicando que a pilha esta vazia.
+ *  \sa push(), pop(), top()
+ */
+template<typename T>
+void ArrayStack<T>::clear() {
+    top_ = -1;
+}
 
-    //! Esvazia a pilha.
-    /*! Atualiza o índice do topo para -1, indicando que a pilha esta vazia.
-     *  \sa push(), pop(), top()
-     */
-    template<typename T>
-    void ArrayStack<T>::clear() {
-        top_ = -1;
-    }
+//! Quantidade de dados na pilha.
+/*! Retorna o tamanho da pilha convertido para size_t, seguindo a lógica
+ *  do índice+1 considerando que o índice inicial em -1.
+ *  \return size_t referente ao tamanho da pilha.
+ *  \sa max_size()
+ */
+template<typename T>
+std::size_t ArrayStack<T>::size() {
+    return (std::size_t) (top_+1);
+}
 
-    //! Quantidade de dados na pilha.
-    /*! Retorna o tamanho da pilha convertido para size_t, seguindo a lógica
-     *  do índice+1 considerando que o índice inicial em -1.
-     *  \return size_t referente ao tamanho da pilha.
-     *  \sa max_size()
-     */
-    template<typename T>
-    std::size_t ArrayStack<T>::size() {
-        return (std::size_t) (top_+1);
-    }
+//! Maior tamanho possível.
+/*! Retorna o maior tamanho possível armazenado na variavel max_size_.
+ *  \return size_t referente ao max_size_.
+ *  \sa size()
+ */
+template<typename T>
+std::size_t ArrayStack<T>::max_size() {
+    return max_size_;
+}
 
-    //! Maior tamanho possível.
-    /*! Retorna o maior tamanho possível armazenado na variavel max_size_.
-     *  \return size_t referente ao max_size_.
-     *  \sa size()
-     */
-    template<typename T>
-    std::size_t ArrayStack<T>::max_size() {
-        return max_size_;
-    }
+//! Pilha vazia
+/*! Testa se a pilha está vazia.
+ *  \return valor booleano.
+ *  \sa full()
+ */
+template<typename T>
+bool ArrayStack<T>::empty() {
+    return top_ == -1;
+}
 
-    //! Pilha vazia
-    /*! Testa se a pilha está vazia.
-     *  \return valor booleano.
-     *  \sa full()
-     */
-    template<typename T>
-    bool ArrayStack<T>::empty() {
-        return top_ == -1;
-    }
-
-    //! Pilha cheia
-    /*! Testa se a pilha está cheia.
-     *  \return valor booleano.
-     *  \sa empty()
-     */
-    template<typename T>
-    bool ArrayStack<T>::full() {
-        return top_+1 == static_cast<int>(max_size_);
-    }
+//! Pilha cheia
+/*! Testa se a pilha está cheia.
+ *  \return valor booleano.
+ *  \sa empty()
+ */
+template<typename T>
+bool ArrayStack<T>::full() {
+    return top_+1 == static_cast<int>(max_size_);
+}
 
 }  // namespace structures
 
