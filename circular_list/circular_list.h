bcd978dd3c14a47748ffc377adf5bfda02420d2b
João Vicente Souto <joaovicentesouto@gmail.com>
2017-04-13 19:27:29 -0300

    Documentação completa

--- circular_list/circular_list.h
+++ circular_list/circular_list.h
@@ -76,9 +76,9 @@ public:
         }
 
         //! Setter do dado
-        /*! Retorna o dado armazenado.
+        /*! Altera o dado;
          *  \sa data() const, data()
-         *  \return T& referência do dado armazenado.
+         *  \param data T& novo dado.
          */
         void data(const T& data) {
             data_ = data;
@@ -147,14 +147,36 @@ public:
     std::size_t size_{0u};  //< size_
 };
 
+//! Construtor padrão
+/*! Sem parâmetros, a lista já contém os valores inicializados já na definição.
+ */
 template<typename T>
 CircularList<T>::CircularList() {}
 
+//! Destrutor
+/*! Método para desalocar a memória utilizada pela lista.
+ */
 template<typename T>
 CircularList<T>::~CircularList() {
   clear();
 }
 
+//! Esvazia a lista.
+/*! Retira todos os Nodes.
+ */
+template<typename T>
+void CircularList<T>::clear() {
+  while (!empty())
+    pop_front();
+}
+
+//! Inserção no fim da lista.
+/*! Sempre será colocado no final da lista, caso não esteja vazia.
+ *  Possíveis erros:
+ *   - Se a lista estiver cheia.
+ *  \param data Dado T que será inserido na fila.
+ *  \sa push_front(), insert(), insert_sorted()
+ */
 template<typename T>
 void CircularList<T>::push_back(const T& data) {
   if (empty()) {
@@ -171,6 +193,13 @@ void CircularList<T>::push_back(const T& data) {
   }
 }
 
+//! Inserção no começo da lista.
+/*! Sempre será colocado no início da fila.
+ *  Possíveis erros:
+ *   - Se a lista estiver cheia.
+ *  \param data Dado T que será inserido na fila.
+ *  \sa push_back(), insert(), insert_sorted()
+ */
 template<typename T>
 void CircularList<T>::push_front(const T& data) {
   Node* new_node = new Node(data);
@@ -189,6 +218,15 @@ void CircularList<T>::push_front(const T& data) {
   size_++;
 }
 
+//! Inserção em qualquer lugar da lista.
+/*! Verificando se a posição for válida, depois inserindo onde se deve.
+ *  Possíveis erros:
+ *   - Se o índice não existir.
+ *   - Se a lista estiver cheia.
+ *  \param data Dado T que será inserido na fila.
+ *  \param index Size_t indicando a posição que será inserido o dado.
+ *  \sa push_back(), push_front(), insert_sorted()
+ */
 template<typename T>
 void CircularList<T>::insert(const T& data, std::size_t index) {
   if (index > size_)
@@ -231,6 +269,16 @@ void LinkedList<T>::insert(const T& data, Node* before) {
     size_++;
 }
 
+//! Inserção ordenada na lista.
+/*! Será buscado a posição ordenada do dado passado por parâmetro. A forma de
+ *  comparação utilizada será o operador ">" que deve ser sobrescrito por quem
+ *  for utilizar a lista e este método.
+ *  Possíveis erros:
+ *   - Se a lista estiver cheia.
+ *  \param data Dado T que será inserido na fila.
+ *  \param Node* Ponteiro do elemento para inserir no próximo.
+ *  \sa push_back(), push_front(), insert()
+ */
 template<typename T>
 void CircularList<T>::insert_sorted(const T& data) {
   if (empty()) {
@@ -253,6 +301,15 @@ void CircularList<T>::insert_sorted(const T& data) {
   }
 }
 
+//! Referencia o dado na posição da lista.
+/*! Retorna o dado que esta na posição index da lista para uso externo,
+ *  caso exista.
+ *  Possíveis erros:
+ *   - Se o índice não existir.
+ *   - Se a lista estiver vazia.
+ *  \param index Size_t índice do node.
+ *  \return T& Dado que será referenciado.
+ */
 template<typename T>
 T& CircularList<T>::at(std::size_t index) {
   if (index >= size())
@@ -262,6 +319,15 @@ T& CircularList<T>::at(std::size_t index) {
   return current->data();
 }
 
+//! Referencia o dado na posição da lista.
+/*! Retorna o dado que esta na posição index da lista para uso externo,
+ *  caso exista.
+ *  Possíveis erros:
+ *   - Se o índice não existir.
+ *   - Se a lista estiver vazia.
+ *  \param index Size_t índice do node.
+ *  \return T& Dado que será referenciado.
+ */
 template<typename T>
 const T& CircularList<T>::at(std::size_t index) const {
   if (index >= size())
@@ -271,6 +337,16 @@ const T& CircularList<T>::at(std::size_t index) const {
   return current->data();
 }
 
+//! Coleta o dado de uma posição específica da lista.
+/*! Verificando se a posição for válida, será removido e a lista reorganizada,
+ *  depois o dado retirado é retornado e o tamanho decrementado.
+ *  Possíveis erros:
+ *   - Se o índice não existir.
+ *   - Se a lista estiver vazia.
+ *  \param index Size_t sendo a posição do dado a ser retirado.
+ *  \return T dado genérico retirado da lista.
+ *  \sa pop_back(), pop_front(), remove()
+ */
 template<typename T>
 T CircularList<T>::pop(std::size_t index) {
   if (empty())
@@ -290,11 +366,27 @@ T CircularList<T>::pop(std::size_t index) {
   return data;
 }
 
+//! Coleta o dado do final da lista
+/*! Sempre será retirado no final da lista.
+ *  Reuso do método pop().
+ *  Possíveis erros:
+ *   - Se a lista estiver vazia.
+ *  \return T dado genérico retirado da lista.
+ *  \sa pop(), pop_front(), remove()
+ */
 template<typename T>
 T CircularList<T>::pop_back() {
   return pop(size_ - 1u);
 }
 
+//! Coleta o dado do início da lista.
+/*! Sempre será retirado o primeiro dado da lista, caso não esteja vazia.
+ *  Reuso do método pop().
+ *  Possíveis erros:
+ *   - Se a lista estiver vazia.
+ *  \return T dado genérico retirado da lista.
+ *  \sa pop(), pop_back(), remove()
+ */
 template<typename T>
 T CircularList<T>::pop_front() {
   if (empty())
@@ -316,21 +408,46 @@ T CircularList<T>::pop_front() {
   return data;
 }
 
+//! Remoção de um dado da lista.
+/*! Busca o índice do dado e remove ele da lista.
+ *  Reuso do método pop() e find();
+ *  Possíveis erros:
+ *   - Se a lista estiver vazia.
+ *   - O dado não foi encontrado.
+ *  \param data Dado T que será removido da lista.
+ *  \sa pop(), pop_back(), pop_front(), find()
+ */
 template<typename T>
 void CircularList<T>::remove(const T& data) {
   pop(find(data));
 }
 
+//! lista vazia
+/*! Testa se a lista está vazia.
+ *  \return um booleano.
+ */
 template<typename T>
 bool CircularList<T>::empty() const {
   return size() == 0;
 }
 
+//! Contém um dado
+/*! Testa se um dado está na lista.
+ *  \param data T& Dado que se deseja testar a sua existência.
+ *  \return um booleano.
+ *  \sa find()
+ */
 template<typename T>
 bool CircularList<T>::contains(const T& data) const {
   return find(data) != size();
 }
 
+//! Procura dado.
+/*! Procura o índice do dado, caso não achar retorna o tamanho da lista.
+ *  \param data T& Dado que se deseja procurar.
+ *  \return um booleano.
+ *  \sa contains()
+ */
 template<typename T>
 std::size_t CircularList<T>::find(const T& data) const {
   std::size_t index = 0u;
@@ -344,15 +461,16 @@ std::size_t CircularList<T>::find(const T& data) const {
   return index;
 }
 
+//! Tamanho da lista.
+/*! Retorna o tamanho (size_) da lista.
+ *  \return size_t o tamanho da lista.
+ */
 template<typename T>
 std::size_t CircularList<T>::size() const {
   return size_;
 }
 
-//! Tamanho da lista.
-/*! Retorna o tamanho (size_) da lista.
- *  \return size_t o tamanho da lista.
- */
+//! Desenha conexoes
 template<typename T>
 void CircularList<T>::draw_connection() {
   auto temp = head;
