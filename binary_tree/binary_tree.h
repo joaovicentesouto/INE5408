10b0f4a849e32f8491eeeb7e4f610c1678c805b0
João Vicente Souto <joaovicentesouto@gmail.com>
2017-05-13 11:36:33 -0300

    Passou em todos os testes até MultipleIntegerInsertion: error: seg... fault

--- binary_tree/binary_tree.h
+++ binary_tree/binary_tree.h
@@ -58,24 +58,91 @@ class BinaryTree {
             }
         }
 
-        //! Doc
-        bool remove(const T& data_);
+        //! Sem o uso de uma segunda função
+        bool remove(const T& data) {
+            // Go to left
+            if (data < data_) {
+                if (left_ != nullptr)
+                    return left_->remove(data);
+                return false;
+            }
+            // Go to right
+            if (data > data_) {
+                if (left_ != nullptr)
+                    return right_->remove(data);
+                return false;
+            }
+            // I found
+            // Two sons
+            if (right_ != nullptr && left_ != nullptr) {
+                data_ = right_->minimum()->data_;
+                return right_->remove(data_);
+            }
+            // One son or leaf
+            if (right_ != nullptr) {
+                data_ = right_->data_;
+                if (right_->right_ != nullptr || right_->left_ != nullptr)
+                    return right_->remove(data_);
+                // Son is a leaf
+                delete right_;
+                right_ = nullptr;
+                return true;
+            } else {
+                data_ = left_->data_;
+                if (left_->right_ != nullptr || left_->left_ != nullptr)
+                    return left_->remove(data_);
+                // Son is a leaf
+                delete left_;
+                left_ = nullptr;
+                return true;
+            }
+        }
 
         //! Doc
-        bool contains(const T& data_) const;
+        bool contains(const T& data) const {
+            if (data < data_)
+                return left_ == nullptr? false : left_->contains(data);
+            else if (data > data_)
+                return right_ == nullptr? false : right_->contains(data);
+            else
+                return true;
+        }
 
         //! Doc
-        void pre_order(ArrayList<T>& v) const;
+        void pre_order(ArrayList<T>& v) const {
+            v.push_back(data_);
+            left_->pre_order(v);
+            right_->pre_order(v);
+        }
 
         //! Doc
-        void in_order(ArrayList<T>& v) const;
+        void in_order(ArrayList<T>& v) const {
+            left_->in_order(v);
+            v.push_back(data_);
+            right_->in_order(v);
+        }
 
         //! Doc
-        void post_order(ArrayList<T>& v) const;
+        void post_order(ArrayList<T>& v) const {
+            left_->post_order(v);
+            right_->post_order(v);
+            v.push_back(data_);
+        }
+
+     private:
+        //! Doc
+        Node* minimum() {
+            if (right_ == nullptr)
+                return this;
+            return right_->minimum();
+        }
     };
 
-    Node* root_;
-    std::size_t size_;
+    //! Doc
+    //  Node* minimum(Node* arv);
+
+    Node* root_{nullptr};
+    std::size_t size_{0};
 };
 
 //! Doc
@@ -91,7 +158,7 @@ BinaryTree<T>::~BinaryTree()
 //! Doc
 template<typename T>
 void BinaryTree<T>::insert(const T& data) {
-    if (root_ == nullptr) {
+    if (empty()) {
         root_ = new Node(data);
         if (root_ == nullptr)
             throw std::out_of_range("Full tree!");
@@ -103,32 +170,106 @@ void BinaryTree<T>::insert(const T& data) {
 
 //! Doc
 template<typename T>
-void BinaryTree<T>::remove(const T& data);
+void BinaryTree<T>::remove(const T& data) {
+    if (empty())
+        throw std::out_of_range("Empty tree!");
+
+    if (root_->right_ != nullptr || root_->left_ != nullptr) {
+        if (root_->remove(data))
+            --size_;
+    } else {
+        // If the root to delete and no sons
+        if (root_->data_ == data) {
+            delete root_;
+            root_ = nullptr;
+            --size_;
+        }
+    }
+}
 
 //! Doc
 template<typename T>
-bool BinaryTree<T>::contains(const T& data) const;
+bool BinaryTree<T>::contains(const T& data) const {
+    if (empty())
+        return false;
+    return root_->contains(data);
+}
 
 //! Doc
 template<typename T>
-bool BinaryTree<T>::empty() const;
+bool BinaryTree<T>::empty() const {
+    return size() == 0;
+}
 
 //! Doc
 template<typename T>
-std::size_t BinaryTree<T>::size() const;
+std::size_t BinaryTree<T>::size() const {
+    return size_;
+}
 
 //! Doc
 template<typename T>
-ArrayList<T> BinaryTree<T>::pre_order() const;
+ArrayList<T> BinaryTree<T>::pre_order() const {
+    ArrayList<T> array{size()};
+    if (empty())
+        return array;
+    root_->pre_order(array);
+    return array;
+}
 
 //! Doc
 template<typename T>
-ArrayList<T> BinaryTree<T>::in_order() const;
+ArrayList<T> BinaryTree<T>::in_order() const {
+    ArrayList<T> array{size()};
+    if (empty())
+        return array;
+    root_->in_order(array);
+    return array;
+}
 
 //! Doc
 template<typename T>
-ArrayList<T> BinaryTree<T>::post_order() const;
+ArrayList<T> BinaryTree<T>::post_order() const {
+    ArrayList<T> v{size()};
+    if (empty())
+        return v;
+    root_->post_order(v);
+    return v;
+}
 
+/* metodo removeção dos slides
+        Node* remove(const T& data, Node* arv) {
+            if (arv == nullptr)
+                return arv;
+            // Go to left
+            if (data < arv->data_) {
+                arv->left_ = remove(data, arv->left_);
+                return arv;
+            }
+            // Go to right
+            if (data > arv->data_) {
+                arv->right_ = remove(data, arv->right_);
+                return arv;
+            }
+            // I found
+            // Two sons
+            if (arv->right_ != nullptr && arv->left_ != nullptr) {
+                Node* temp = arv->right_->minimum();
+                arv->data_ = temp->data_;
+                arv->right_ = remove(data, arv->right_);
+                return arv;
+            }
+            // One son or leaf
+            Node* temp = nullptr;
+            if (arv->right_ != nullptr)
+                temp = arv->right_;
+            else
+                temp = arv->left_;
+
+            delete arv;
+            return temp;
+        }
+*/
 }  // namespace structures
 
 #endif
