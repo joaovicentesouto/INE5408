f66f41c855e55f527db03e05b1d67b5e7166812c
João Vicente Souto <joaovicentesouto@gmail.com>
2017-06-03 21:50:38 -0300

    Deixando mais legível

--- on-disk_data_structures/kd_tree_on_disk.h
+++ on-disk_data_structures/kd_tree_on_disk.h
@@ -28,6 +28,7 @@ public:
   //bool contains_secondary(const char* secondary) const;
   bool empty() const;
   size_t size() const;
+  size_t file_size() const;
 
   //size_t search_primary_key(const char* wanted) const;
   //ArrayList<T> search_secondary_key(const char* wanted) const;
@@ -74,6 +75,7 @@ private:
 };
 
 KDTreeOnDisk::KDTreeOnDisk() {
+  // Cria arquivo para a arvore ou sobreescreve um existente
   fstream tree("./tree.dat", ios::in | ios::out | ios::binary | ios::trunc);
   Node empty;
   tree.write(reinterpret_cast<char*>(&empty), sizeof(Node));
@@ -85,11 +87,10 @@ KDTreeOnDisk::~KDTreeOnDisk() {}
 void KDTreeOnDisk::insert(const char* key_1,
                           const char* key_2,
                           const size_t offset) {
-  fstream tree;
-  tree.open("./tree.dat", ios::in | ios::out | ios::binary);
+  fstream tree("./tree.dat", ios::in | ios::out | ios::binary);
 
   char primary[100], secondary[100];
-  int compare_x = 0, compare_y = 0, left = false;
+  int compare_x = 0, compare_y = 0;
   size_t offset_tree = 0u, direction, level = 0u;
 
   tree.seekg(0);
@@ -99,46 +100,32 @@ void KDTreeOnDisk::insert(const char* key_1,
     tree.read(primary, sizeof(primary));
     tree.read(secondary, sizeof(secondary));
     tree.seekp(offset_tree);
-    cout << "p: " << primary << " s: " << secondary << endl;
+    //cout << "p: " << primary << " s: " << secondary << endl;
 
-    if (primary[0] == '@') {
+    if (primary[0] == '@') { // inserindo
       Node *tnode = new Node(key_1, key_2, offset);
       if (tnode == nullptr)
         throw std::out_of_range("Full tree!");
 
-      cout << offset_tree << " " << size_ << endl;
       tree.write(reinterpret_cast<char*>(tnode), sizeof(Node));
       ++size_;
       delete tnode;
+
       break;
     }
 
     compare_x = strcmp(key_1, primary);
     compare_y = strcmp(key_2, secondary);
 
-    cout << "x: " << compare_x << " y: " << compare_y << endl;
-
     if (compare_x == 0 && compare_y == 0) // node ja existe
       break;
 
-    if ((level % 2) == 0) { // x
-      left = (compare_x < 0 || (compare_x == 0 && compare_y < 0));
-      if (left) {
-        direction = 1;
-      } else {
-        direction = 2;
-      }
-    } else { // y
-      left = compare_y < 0 || (compare_y == 0 && compare_x < 0);
-      if (left) {
-        direction = 1;
-      } else {
-        direction = 2;
-      }
-    }
+    if ((level % 2) == 0) // 1ª dimensão
+      direction = compare_x < 0 || (compare_x == 0 && compare_y < 0)? 1 : 2;
+    else // 2ª dimensão
+      direction = compare_y < 0 || (compare_y == 0 && compare_x < 0)? 1 : 2;
 
     tree.seekg(2*offset_tree + sizeof(Node)*direction);
-    //cout << "tellg: " << tree.tellg() << endl;
     ++level;
   }
   tree.close();
@@ -147,14 +134,14 @@ void KDTreeOnDisk::insert(const char* key_1,
 
 void KDTreeOnDisk::new_level(const size_t level) {
   if (level == depth_) {
-    //ofstream tree("./tree.dat", ios::out | ios::binary);
-    ofstream tree("tree.dat", std::ios_base::app | ios::binary);
+
+    ofstream tree("./tree.dat", std::ios_base::app | ios::binary);
     tree.seekp((pow(2,(depth_+1))-1)*sizeof(Node));
+
     Node tnode;
-    for (size_t i = 0; i < pow(2,(depth_+1)); ++i) {
-      //cout << "onde level vai escrever: " << tree.tellp() << endl;
+    for (size_t i = 0; i < pow(2,(depth_+1)); ++i)
       tree.write(reinterpret_cast<char*>(&tnode), sizeof(tnode));
-    }
+
     depth_++;
   }
 }
@@ -162,11 +149,16 @@ void KDTreeOnDisk::new_level(const size_t level) {
 bool KDTreeOnDisk::empty() const {
   return size_ == 0u;
 }
+
 size_t KDTreeOnDisk::size() const {
-  struct stat st;
-  if (stat("./tree.dat", &st) == 0)
-    cout << st.st_size << endl;
   return size_;
+}
+
+size_t KDTreeOnDisk::file_size() const {
+  struct stat st;
+  if (stat("./tree.dat", &st) != 0)
+    throw std::out_of_range("Erro ao verificar tamanho do arquivo.");
+  return st.st_size;
 }
 
 }  //  namespace structures
--- on-disk_data_structures/main.cpp
+++ on-disk_data_structures/main.cpp
@@ -18,17 +18,12 @@ int main(int argc, char const *argv[]) {
   cout << "Tamanho antes: " << tree.size() << endl;
 
   tree.insert((char*)"c", (char*)"c", 0);
-  tree.insert((char*)"b", (char*)"b", 1);
+  tree.insert((char*)"a", (char*)"a", 1);
   tree.insert((char*)"d", (char*)"d", 2);
-  tree.insert((char*)"a", (char*)"a", 3);
-  tree.insert((char*)"e", (char*)"e", 4);
+  tree.insert((char*)"b", (char*)"b", 3);
+  tree.insert((char*)"c", (char*)"d", 4);
 
   cout << "Tamanho depois: " << tree.size() << endl;
-
-  ifstream fin("./tree.dat", ios::in | ios::binary);
-  char t[100];
-  fin.read(t, sizeof(t));
-  cout << "Primeira posicao da arvore depois de acabar: " << t << endl;
 
   return 0;
 }
