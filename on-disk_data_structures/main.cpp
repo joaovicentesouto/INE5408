0fd7dfa0c92cba906fcaa418fbfe45128b777b22
João Vicente Souto <joaovicentesouto@gmail.com>
2017-06-04 21:59:10 -0300

    Mais alguns testes, insert parece estar funcionando perfeitamente

--- on-disk_data_structures/main.cpp
+++ on-disk_data_structures/main.cpp
@@ -18,10 +18,13 @@ int main(int argc, char const *argv[]) {
   cout << "Tamanho antes: " << tree.size() << endl;
 
   tree.insert((char*)"c", (char*)"c", 0);
-  tree.insert((char*)"a", (char*)"a", 1);
-  tree.insert((char*)"d", (char*)"d", 2);
-  tree.insert((char*)"b", (char*)"b", 3);
-  tree.insert((char*)"c", (char*)"d", 4);
+  tree.insert((char*)"c", (char*)"d", 1);
+  tree.insert((char*)"d", (char*)"a", 2);
+  tree.insert((char*)"a", (char*)"b", 3);
+  tree.insert((char*)"a", (char*)"a", 4);
+  tree.insert((char*)"b", (char*)"c", 5);
+  tree.insert((char*)"b", (char*)"d", 6);
+  tree.insert((char*)"b", (char*)"b", 7);
 
   cout << "Tamanho depois: " << tree.size() << endl;
 
